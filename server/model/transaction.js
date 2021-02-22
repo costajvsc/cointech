7d39ae33ed98267c93a718903c1d3cfa56693847
João Victor (Mad) <costa.jvsc@gmail.com>
2021-02-22 15:37:35 -0300

    [SERVER] - Implement model for Transaction and Users

---
+++ server/model/transaction.js
@@ -0,0 +1,15 @@
+const user = {
+    _id: 1,
+    userId: 1,
+    title: '', 
+    amount: 0,
+    type: '',
+    category: '',
+    budget: '',
+    payment: '',
+    date: new Date(),
+    created_at: new Date(),
+    edited_at: new Date(),
+}
+
+module.exports = user
---
+++ server/model/user.js
@@ -0,0 +1,12 @@
+const user = {
+    _id: '',
+    fristName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    category: [],
+    budgets: [],
+    payments: []
+}
+
+module.exports = user;
